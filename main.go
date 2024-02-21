b8bf80f15d8c39578b844a48089e1c41a9a90015
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2024-02-21 11:16:23 +0700

    membuat struktur folder dan konfigurasi

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"egardev-gin-socmed/config"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	config.LoadConfig()
+	config.LoadDB()
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	api := r.Group("/api")
+
+	api.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+
+	r.Run(fmt.Sprintf(":%v", config.ENV.PORT))
 }
